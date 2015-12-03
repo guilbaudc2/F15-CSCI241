1d9efe76b55cc7e47478164cccf9c059118f1587
Catherine Guilbaud <guilbaudc2@mailbox.winthrop.edu>
2015-12-03 12:34:44 -0500

    Re-edited the wording on the homepage.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once("header.php");
 
 ?>
 <p>Hello and welcome to the university's event hub, where you can see what events will be taking place and their location. You can also add the events to a list and
-	send yourself a friend email reminders of the events you'd like to attend.</p>
+	email the events you'd like to attend to yourself or a friend.</p>
 	
 <p>If you are an administrator, please log in.</p>
 <?php
