ce6b665826fbe970ef05b1d280d5613f7e50cd1b
Catherine Guilbaud <guilbaudc2@mailbox.winthrop.edu>
2015-12-07 21:08:22 -0500

    Edited some spacing on the index.php file.

--- project/index.php
+++ project/index.php
@@ -14,7 +14,7 @@
  * they're writing about a Japanese film, that entry will be added to the japfilms.txt file and can be seen if people go to the Japanese page).
  * Because of the way the functions work, if someone wants to post a new entry, all the information is required ()
  * 
- *  The admin can delete or add new entries (they'll be taken to their own page which is similar to the guest blogger page except all the entries have a 
+ * The admin can delete or add new entries (they'll be taken to their own page which is similar to the guest blogger page except all the entries have a 
  * hidden input that allows for deletion). Also, they can go to both the Japanese and Korean pages and delete all comments. 
  * 
  *
